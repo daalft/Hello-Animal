6b0240574382a0f87e581ac1f49fc5acaf3a330f
David Alfter <blueygh2@gmail.com>
2011-10-27 08:33:41 +0200

    class animal commit

---
+++ main.rb
@@ -0,0 +1,25 @@
+class Tier
+  
+  @name
+  @sound
+  
+  def initialize name
+    @name = name.capitalize
+    get_sound
+  end
+  
+  def get_sound 
+    puts "Wie macht ein(e) #{@name}?".delete("\n")
+    @sound = gets
+  end
+  
+  def say
+    puts "#{@name} macht #{@sound.downcase}...".delete("\n")
+  end
+  
+end
+
+puts "Geben Sie ein Tier ein:"
+x = gets
+tier = Tier.new(x)
+tier.say
